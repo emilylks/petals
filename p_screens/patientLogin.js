596231faf980168ac64a3293de69394c3e9cd423
Ethan <56213065+Scholarethan@users.noreply.github.com>
2020-09-05 02:44:58 -0700

    Update and rename p_login.js to patientLogin.js
    Ethan finished the patient login screen

--- p_screens/patientLogin.js
+++ p_screens/patientLogin.js
@@ -1,21 +1,19 @@
-/* Patient login */
-
 import React from 'react';
 import {View, Text, Image, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
 
-const App = () => {
+function patientLogin() {
   return (
     <View style={styles.background}>
       <View style={styles.header}>
         <Text style={styles.headerText}>
-          PROVIDER LOGIN
+          PATIENT LOGIN
         </Text>
       </View>
       <View style={styles.container}> 
         <Text style={styles.promptText}> 
-          ORGANIZATION:
+          ID:
         </Text>
-        <TextInput style={styles.input} placeholder = 'Enter your organization name'>
+        <TextInput style={styles.input} placeholder = 'Enter your provider issued ID'>
         </TextInput>
       </View>
       <View style={styles.container}> 
@@ -23,6 +21,13 @@ const App = () => {
           NAME (LAST, FIRST):
         </Text>
         <TextInput style={styles.input} placeholder = 'Last, first name'>
+        </TextInput>
+      </View>
+      <View style={styles.container}> 
+        <Text style={styles.promptText}>
+          DATE OF BIRTH (DD/MM/YYYY):
+        </Text>
+        <TextInput style={styles.input} placeholder = 'DD/MM/YYYY'>
         </TextInput>
       </View>
       <View style={styles.container}> 
@@ -39,9 +44,11 @@ const App = () => {
         <TextInput style={styles.input} placeholder = 'Enter your password'>
         </TextInput>
       </View>
-      <View style={styles.confirmationButton}>
-
-      </View>
+      <TouchableOpacity style={styles.confirmationButton}>
+        <Text style={{fontSize: 20, textAlign: 'center', color: 'white'}}>
+          SIGN IN
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -53,7 +60,7 @@ const styles = StyleSheet.create({
     width: 415,
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 75,
+    marginBottom: 30,
   },
   container: {
     justifyContent: 'center',
@@ -82,17 +89,27 @@ const styles = StyleSheet.create({
   },
   input: {
     backgroundColor: '#A2E1E2',
-        borderRadius: 30,
-        borderWidth: 1,
-        borderColor: '#CDCDC6',
-        width: 310,
-        height: 45,
-        textAlign: 'center',
-        fontStyle: 'italic',
-        fontSize: 16,
-        // letterSpacing: 4,
+    borderRadius: 30,
+    borderWidth: 1,
+    borderColor: '#CDCDC6',
+    width: 310,
+    height: 45,
+    textAlign: 'center',
+    fontStyle: 'italic',
+    fontSize: 16,
+    // letterSpacing: 4,
+  },
+  confirmationButton: {
+    marginTop: 10,
+    justifyContent: 'center',
+    backgroundColor: '#4C6663',
+    borderRadius: 30,
+    borderWidth: 1,
+    borderColor: '#CDCDC6',
+    width: 160,
+    height: 60,
   }
 });
 
-export default App;
+export default patientLogin;
 
